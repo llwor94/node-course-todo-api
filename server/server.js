e5dc45b05383a932b952b81257717b02ceea9396
Lauren <llwor94@gmail.com>
2018-11-04 19:55:01 -0800

    add get todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -24,10 +24,6 @@ app.post('/todos', (req, res) => {
 	);
 });
 
-app.listen(3000, () => {
-	console.log('Started on port 3000');
-});
-
 app.get('/todos', (req, res) => {
 	Todo.find().then(
 		todos => {
@@ -41,4 +37,27 @@ app.get('/todos', (req, res) => {
 	);
 });
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id)
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+
+			res.send(todo);
+		})
+		.catch(e => {
+			res.status(400).send();
+		});
+});
+
+app.listen(3000, () => {
+	console.log('Started on port 3000');
+});
 module.exports = { app };

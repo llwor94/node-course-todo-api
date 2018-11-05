bd395a62ae45c5fd2ce1c1d1e10a1c2fdbfe41b4
Lauren <llwor94@gmail.com>
2018-11-04 19:30:50 -0800

    add get /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,10 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+beforeEach(done => {
+	Todo.remove({}).then(() => done());
+});
+
 describe('Post /todos', () => {
 	it('should create a new todo', done => {
 		var text = 'Test todo text';
@@ -28,5 +32,20 @@ describe('Post /todos', () => {
 					})
 					.catch(e => done(e));
 			});
+	});
+
+	it('should not create todo with invalid body data', done => {
+		request(app).post('/todos').send({}).expect(400).end((err, res) => {
+			if (err) {
+				return done(err);
+			}
+
+			Todo.find()
+				.then(todos => {
+					expect(todos.length).toBe(0);
+					done();
+				})
+				.catch(e => done(e));
+		});
 	});
 });
